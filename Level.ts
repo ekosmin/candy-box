2feb61b4416fab1b2f07441f91efc2d6a9cbd559
Eli Kosminsky <ekosmin@mit.edu>
2015-03-17 11:26:27 -0400

    Initial level/Phaser commit

---
+++ Level.ts
@@ -0,0 +1,12 @@
+///<reference path="build/typescript/phaser.d.ts"/>
+module Main {
+
+    export class Level extends Phaser.State {
+
+        create() {
+            console.log("Level loaded!");
+        }
+
+    }
+
+}
